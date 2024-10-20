583526addc1d076f2e0202fdf7eb2dc2625c89c1
Aneesh <90706436+007aneesh@users.noreply.github.com>
2024-10-20 18:54:30 +0530

    Delete firebase/firebaseAdmin.ts

--- firebase/firebaseAdmin.ts
+++
@@ -1,9 +0,0 @@
-const firebase = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  storageBucket: "gs://devcase-2a759.appspot.com"
-});
-
-const storage = firebase.storage();
